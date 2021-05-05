e1d4e118909b6818c9d8a38ea20d427d35760fea
pahuja0 <83006044+pahuja0@users.noreply.github.com>
2021-05-05 17:56:14 +0530

    commit -m

--- CREATEJsonFile2.java
+++ CREATEJsonFile2.java
@@ -9,14 +9,7 @@ public class CREATEJsonFile2
     @SuppressWarnings("unchecked")
     public static void main( String[] args )
     {
-        //First Employee
-        JSONObject employeeDetails = new JSONObject();
-        employeeDetails.put("firstName", "Lokesh");
-        employeeDetails.put("lastName", "Gupta");
-        employeeDetails.put("website", "howtodoinjava.com");
-         
-        JSONObject employeeObject = new JSONObject(); 
-        employeeObject.put("employee", employeeDetails);
+        
          
         //Second Employee
         JSONObject employeeDetails2 = new JSONObject();
